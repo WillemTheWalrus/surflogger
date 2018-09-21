9119c82b3c96512d989ccdad6c72b174ef2778b9
William Doudna <billdoudna@gmail.com>
2018-09-21 05:44:35 +0000

    * experimenting with cookie generation * made a config file for dbconnect and future config files * fixed error display bug in layout

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,10 +5,40 @@ const { sanitizeBody } = require('express-validator');
 
 
 
-exports.login_auth = function(req, res, next) {
-	res.render('home');
-};
 
+exports.login_auth = function(req, res, next) {
+	Account.findOne({'username' : req.body.username}, 'username password' , function(err, results){
+		if(err){
+			res.send(error);
+		}
+		else{
+			if(req.body.password == results.password){
+
+				//create a random number as our session ID and store it in a cookie
+				//Everytime we go to a new page after logging in we will check to make sure the 
+				//session id in our url matches the stored session ID in our cookie
+				
+				var sessionID = Math.floor(Math.random()*10000000)+1);
+				var cookieName = req.body.username + '::session';
+				
+				res.cookie(cookieName, sessionID, {expire: 360000 + Date.now()});
+
+				
+
+				
+			}
+			else
+			{
+				res.render('index', { errors: 'Incorrect password' } );
+			}	
+		}
+	});
+
+}
+
+exports.render_home = function(req,res,next) {
+
+	req.cookies
 
 exports.account_create = function(req, res, next) {
 
